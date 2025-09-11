4fced9c018f54c9229413637bd43144f6682a38a
Stefan Wiesinger <stefanwiesinger2005@gmail.com>
2025-09-11 02:20:02 +0200

    fix basepath

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
 // App constants
 const rawBasePath = process.env.NEXT_PUBLIC_BASE_PATH || '/';
-export const BASE_PATH = rawBasePath === '/' ? '' : rawBasePath;
+// When Next.js basePath is set, client-side BASE_PATH should be empty
+// Next.js automatically handles basePath for internal routing
+export const BASE_PATH = '';
