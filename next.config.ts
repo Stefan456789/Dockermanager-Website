e3af47d13eff8a7f0797c7f2fb7a21d62e2ca90b
Stefan Wiesinger <stefanwiesinger2005@gmail.com>
2025-09-11 02:12:28 +0200

    fix basepath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.BASE_PATH || '/';
-const assetPrefix = process.env.BASE_PATH || '/';
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
+const assetPrefix = process.env.NEXT_PUBLIC_BASE_PATH;
 
 const nextConfig = {
   basePath: basePath === '/' ? '' : basePath,
